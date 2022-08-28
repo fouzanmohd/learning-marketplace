d19039c624d9d2411157ba1fd8d7f10aa8b896da
Muhammed Fouzan <fouzanpharmd@gmail.com>
2022-08-28 22:31:05 +0100

    client design initiated

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () =>{
+    return (
+        <>
+        <h1 class="jumbotron bg-primary text-center square">Online Learning Marketplace</h1>
+        <p>Home Page</p>
+        </>
+    )
+}
+
+export default Index;
---
+++ client/pages/login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="jumbotron bg-primary text-center square">Login</h1>
+      <p>Login Page</p>
+    </>
+  );
+};
+export default Login;
